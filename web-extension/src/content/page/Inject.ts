1e3cf3bb792f7920d401c8b679972da2d56b9ef6
Sergei Sergeev <sergeev.srg@gmail.com>
2021-01-26 00:11:08 +0300

    Fixed OUIF issue for SharePoint 2019.

--- web-extension/src/content/page/Inject.ts
+++ web-extension/src/content/page/Inject.ts
@@ -3,6 +3,19 @@ import { ColumnFormatterSettings } from './components/ColumnFormatterSettings';
 import { enableFormatter } from './ColumnFormatterEnhancer';
 import { DomService, ViewType } from './services/DomService';
 import { getListFields } from './services/SPService';
+import { GlobalSettings } from '@uifabric/utilities/lib/GlobalSettings';
+import { getTheme } from '@uifabric/styling/lib/styles/theme';
+
+// if SP 2019
+if (!(window as any)._spPageContextInfo.isSPO) {
+  // Fix to make it work with OUIFR 7.x
+
+  const customizations = GlobalSettings.getValue('customizations');
+  const theme = getTheme();
+  (customizations as any).settings.theme.effects = { ...theme.effects };
+  (customizations as any).settings.theme.spacing = { ...theme.spacing };
+  (customizations as any).settings.theme.fonts = { ...theme.fonts };
+}
 
 // prefetch fields - for performance - they will be cached
 getListFields();

82422d9b8501778367c3892af91476591f407a0a
Chris Kent <8364109+thechriskent@users.noreply.github.com>
2021-04-25 13:46:41 -0400

    Fixed Context Clone Error
    The emit calls with the _spPageContextInfo were failing because the clone operation only does a shallow clone. Instead of passing the entire context, only those values needed are passed. Less convenient for future changes, but protects against breaking additions to context in SPO.

--- web-extension/src/content/page/SpPageContextProvider.ts
+++ web-extension/src/content/page/SpPageContextProvider.ts
@@ -1,11 +1,12 @@
 import { Content } from '../../common/events/Events';
 import { WebEventEmitter } from '../../common/events/WebEventEmitter';
+import { IPageContextInfo } from '../../typings';
 
 const pagePipe = WebEventEmitter.instance;
 
 pagePipe.on(Content.onGetSpPageContextInfo, async () => {
   if (window._spPageContextInfo) {
-    pagePipe.emit(Content.onSendSpPageContextInfo, window._spPageContextInfo);
+    pagePipe.emit(Content.onSendSpPageContextInfo, {isSPO: window._spPageContextInfo.isSPO} as IPageContextInfo);
   } else if (window.moduleLoaderPromise) {
     const ctx = await window.moduleLoaderPromise;
     const legacyContext = ctx.context.pageContext.legacyPageContext;

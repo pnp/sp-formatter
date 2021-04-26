b71a3fc695364ebf19763cd5a92daca019a1393e
Sergei Sergeev <sergeev.srg@gmail.com>
2021-04-26 22:21:37 +0300

    Fixed legacy context as well.

--- web-extension/src/content/page/SpPageContextProvider.ts
+++ web-extension/src/content/page/SpPageContextProvider.ts
@@ -6,12 +6,12 @@ const pagePipe = WebEventEmitter.instance;
 
 pagePipe.on(Content.onGetSpPageContextInfo, async () => {
   if (window._spPageContextInfo) {
-    pagePipe.emit(Content.onSendSpPageContextInfo, {isSPO: window._spPageContextInfo.isSPO} as IPageContextInfo);
+    pagePipe.emit(Content.onSendSpPageContextInfo, { isSPO: window._spPageContextInfo.isSPO } as IPageContextInfo);
   } else if (window.moduleLoaderPromise) {
     const ctx = await window.moduleLoaderPromise;
     const legacyContext = ctx.context.pageContext.legacyPageContext;
     window._spPageContextInfo = legacyContext;
-    pagePipe.emit(Content.onSendSpPageContextInfo, legacyContext);
+    pagePipe.emit(Content.onSendSpPageContextInfo, { isSPO: legacyContext.isSPO } as IPageContextInfo);
   } else {
     throw new Error('Unable to resolve page context info');
   }

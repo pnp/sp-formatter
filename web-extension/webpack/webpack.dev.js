a011f0211c5fc77a060c8beb6be5c4a09e956839
Sergei Sergeev <sergeev.srg@gmail.com>
2021-12-11 17:36:50 +0300

    Added eslint via webpack plugin.

--- web-extension/webpack/webpack.dev.js
+++ web-extension/webpack/webpack.dev.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge').merge;
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'none',
@@ -8,6 +9,10 @@ module.exports = merge(common, {
     new webpack.SourceMapDevToolPlugin({
       filename: null,
       exclude: /monaco-build/
+    }),
+    new ESLintPlugin({
+      lintDirtyModulesOnly: true,
+      files: './src/**/*.{ts,tsx}'
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
--- web-extension/webpack/webpack.prod.js
+++ web-extension/webpack/webpack.prod.js
@@ -1,10 +1,14 @@
 const merge = require('webpack-merge').merge;
 const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
+    new ESLintPlugin({
+      files: './src/**/*.{ts,tsx}'
+    }),
     new BundleAnalyzerPlugin({ generateStatsFile: true, statsFilename: "../../analyze/stats.json", openAnalyzer: false, analyzerMode: "static", reportFilename: "../../analyze/report.html" })
   ]
 });

0a8d4ab071005110a77d3edf5803fcd3bc029aa9
Sergei Sergeev <sergeev.srg@gmail.com>
2021-12-11 16:25:56 +0300

    Removed legacy code from online version.

--- web-extension/src/common/Logger.ts
+++ web-extension/src/common/Logger.ts
@@ -1,7 +1,25 @@
 export class Logger {
-  public static log(info: string): void {
+  public static log(info: string, data?: any): void {
     if (process.env.NODE_ENV === 'development') {
-      console.log(info);
+      const now = new Date();
+      console.log(`[${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [${this.getScriptName()}]: ${info}`);
+      if (data) {
+        console.log(data);
+      }
     }
+  }
+
+  private static getScriptName() {
+    const error = new Error();
+    let source: any;
+    const lastStackFrameRegex = new RegExp(/.+\/(.*?):\d+(:\d+)*$/)
+    const currentStackFrameRegex = new RegExp(/getScriptName \(.+\/(.*):\d+:\d+\)/);
+
+    if ((source = lastStackFrameRegex.exec(error.stack.trim())) && source[1] != '')
+      return source[1];
+    else if ((source = currentStackFrameRegex.exec(error.stack.trim())))
+      return source[1];
+    else if ((error as any).fileName)
+      return (error as any).fileName;
   }
 }
